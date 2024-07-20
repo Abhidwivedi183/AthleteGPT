a151d4a0535f7de2081042203b1b1e7cb2a06910
Abhidwivedi183 <abhishekdwivedi28062003@gmail.com>
2024-07-20 12:56:47 +0100

    first commit

---
+++ src/Openai.js
@@ -0,0 +1,47 @@
+
+
+// const apikey = "sk-proj-aaMfdvJX0tMaodTPRt1iT3BlbkFJSWfBhD57d6nEa7AR5p3y";
+
+// export async function getmessage(){
+//     const options = {
+//         method:'POST',
+//         headers:{
+//             'Authorization':`Bearer ${apikey}`,
+//             'Content-Type':'application/json'
+//         },
+//         body: JSON.stringify({
+//             model: "gpt-4-turbo",
+//             messages: [
+//               {
+//                 role: "user",
+//                 content: "What'\''s the weather like in Boston today?"
+//               }],
+//               max_tokens:100
+//         })
+
+//     }
+//     try{
+// const response = await fetch('https://api.openai.com/v1/chat/completions',options);
+// const data  = await response.json();
+// console.log(data);
+//     }
+//     catch(error) {
+//         console.error(error);
+
+//     }
+// }
+
+export const api = [{
+    name:"Ronaldo",
+    image:"",
+    about:"helooo gjkejdsjhdsjchds"
+},];
+
+
+
+
+ 
+
+
+
+
